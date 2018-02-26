22e394f11b804e1d616f8f9bfa854b02f85445d6
Lucus Xu <liqiuxul5d@gmail.com>
2018-02-26 19:02:49 +0800

    add new

--- env.js
+++ env.js
@@ -2,4 +2,11 @@ module.exports = {
     'env' : 'online',
     'debug' : true,
     'logs_root' : '/home/work/logs/node',
+    'database' : {
+        'database' : 'crawler',
+        'host' : '127.0.0.1',
+        'port' : 3306,
+        'user' : 'root',
+        'password': '',
+    }
 }
--- work/douban/spider.js
+++ work/douban/spider.js
@@ -1,13 +1,17 @@
 const moment = require("moment-timezone");
 const co = require('co');
+const cheerio = require('cheerio');
 const urlencode = require('urlencode');
 const sync = require('../../lib/sync');
 const log = require('../../lib/log');
+const crawler = require('../../lib/crawler');
 const string = require('../../lib/string');
 const util = require('./util');
 const env_conf = require('../../env');
 const logger = new log('douban');
 const work = module.exports = {};
+const server = new crawler().set_time_out(30);
+const book_tag = require('../../models/book_tag');
 
 work.fetch_tags = function () {
     return sync.job(function* () {
@@ -17,6 +21,7 @@ work.fetch_tags = function () {
 
 work.get_tags = function* () {
     var url = "https://book.douban.com/tag/?view=type&icn=index-sorttags-all"
+    console.log(url);
     let html = yield server.fetch(url);
     var $ = cheerio.load(html);
     if ($('div[class="article"]').children('div[class=""]').length == 0) {
@@ -25,25 +30,25 @@ work.get_tags = function* () {
     var tag_list = [];
     $('div[class="article"]').children('div[class=""]').children('div[class=""]').each(function() {
         let className = $(this).children('a').attr('name');
-        console.log(className)
         $(this).find('td').each(function() {
             let tag = $(this).children('a').text()
             let number = $(this).children('b').text()
-            sum = sum.replace('(', '')
+            let sum = number.replace('(', '')
             sum = sum.replace(')', '')
             // 写库
+            var nurl = 'https://book.douban.com/tag/' + urlencode(tag)
+            var tag_info = {
+                "tag" : tag,
+                "classify" : className,
+                "sum" : Number(sum),
+                "site" : "douban",
+                "url" : nurl,
+            }
+            tag_list.push(tag_info);
         });
     });
-}
-
-work.insert = function* (className, tag, sum) {
-    var url = 'https://book.douban.com/tag/' + urlencode(tag)
-    var tag_info = {
-        "tag" : tag,
-        "classify" : className,
-        "sum" : Number(sum),
-        "site" : "douban",
-        "url" : url,
+    for (let info of tag_list) {
+        console.log(info)
+        let result = yield sync.do(book_tag, info);
     }
-    let result = yield sync.do(book_tag, tag_info);
 }
